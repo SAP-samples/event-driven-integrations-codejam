5bcf0b130f149ca34e4d0ee89dabd70b48d176c7
Antonio Maradiaga <antonio@maradia.ga>
2024-04-07 08:32:36 +0200

    Cleaning up topic publisher

--- exercises/10-consume-message-from-CAP/code/edi-codejam-cap-processor/srv/topicPublisher.js
+++ exercises/10-consume-message-from-CAP/code/edi-codejam-cap-processor/srv/topicPublisher.js
@@ -13,30 +13,7 @@ function publishMessageToTopic(payload, topic) {
    * Create CloudEvent message and publish to topic
    ***************************/
 
-  const ce = new CloudEvent({
-    specversion: "1.0",
-    type: "com.sap.codejam.edi.maildelivery.processed",
-    source: "https://codejam-edi-cap-processor.cfapps.eu10.hana.ondemand.com/",
-    data: payload
-  });
-
-  const { headers, body } = HTTP.structured(ce);
-
-  // Add Authorization to headers
-  headers["Authorization"] = `Basic ${base64Credentials}`;
-
-  const postData = JSON.stringify(JSON.parse(body), null, 2);
-
-  LOG.debug("Headers: ", JSON.stringify(headers, null, 2));
-  LOG.debug("Body: ", postData);
-
-  const options = {
-    hostname: process.env.SOLACE_REST_HOST,
-    port: process.env.SOLACE_REST_PORT,
-    path: `/${topic}`,
-    method: "POST",
-    headers: headers,
-  };
+  // INSERT CODE HERE
 
   try {
     const req = https.request(options, (res) => {
